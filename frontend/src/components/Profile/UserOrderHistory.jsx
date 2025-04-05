116492313f3c23b1167dc3842d8f8dd98038dfca
Jay14122004 <162833566+Jay14122004@users.noreply.github.com>
2025-04-05 11:13:48 +0530

    Update UserOrderHistory.jsx

--- frontend/src/components/Profile/UserOrderHistory.jsx
+++ frontend/src/components/Profile/UserOrderHistory.jsx
@@ -14,7 +14,7 @@ function UserOrderHistory() {
   useEffect(() => {
     const fetch = async()=>{
       const response = await axios.get(
-        "http://localhost:1000/api/v1/get-order-history",
+        "https://bookheaven-rpnh.onrender.com/api/v1/get-order-history",
         {headers}
       );
       setOrderHistory(response.data.data);
