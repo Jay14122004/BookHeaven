44b4c0ddb540ac6afa0996472073ffe02558134c
Jay14122004 <162833566+Jay14122004@users.noreply.github.com>
2025-04-05 11:12:23 +0530

    Update Favourites.jsx

--- frontend/src/components/Profile/Favourites.jsx
+++ frontend/src/components/Profile/Favourites.jsx
@@ -15,7 +15,7 @@ function Favourites() {
 
   useEffect(() => {
     const fetch = async ()=>{
-      const response = await axios.get("http://localhost:1000/api/v1/get-favourite-books",
+      const response = await axios.get("https://bookheaven-rpnh.onrender.com/api/v1/get-favourite-books",
         {headers}
       );
       // console.log(response.data.data);
