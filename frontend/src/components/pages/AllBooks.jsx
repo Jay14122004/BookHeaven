9541a36784f6d7a1b49704d020d0bd257e8c6fcd
Jay14122004 <162833566+Jay14122004@users.noreply.github.com>
2025-04-05 11:15:31 +0530

    Update AllBooks.jsx

--- frontend/src/components/pages/AllBooks.jsx
+++ frontend/src/components/pages/AllBooks.jsx
@@ -9,7 +9,7 @@ function AllBooks() {
   const [Data, setData] = useState();
     useEffect(() => {
       const fetch = async()=>{
-        const response = await axios.get("http://localhost:1000/api/v1/get-all-books");
+        const response = await axios.get("https://bookheaven-rpnh.onrender.com/api/v1/get-all-books");
         setData(response.data.data);
       };
       fetch();
